1256f5e83d9ee03937d429224ad53f4075e136e2
Mentoronto <mentorberbatov@gmail.com>
2022-02-15 10:14:18 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Layout from '../component/Layout';
+import { SessionProvider } from "next-auth/react"
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({
+   Component, 
+   pageProps: { session, ...pageProps },
+   }) {
+  return(
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  
+  );
 }
-
-export default MyApp
