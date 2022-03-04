dcc0a2d4c67715160c53617b38a8c2aa65994103
Mentoronto <mentorberbatov@gmail.com>
2022-03-03 21:55:24 -0500

    googleSlice Update

--- Redux/store.js
+++ Redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logInOutReducer from "./googleSlice";
+import logInOutReducer from "../component/googleSlice";
 
 export default configureStore({
   reducer:{
