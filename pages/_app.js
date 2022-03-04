a334061de7d595e420c3b22012302d0684839f61
Mentoronto <mentorberbatov@gmail.com>
2022-03-03 22:00:06 -0500

    toastify remove

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 import { ToastContainer } from "react-toastify";
 import store from "../Redux/store";
 import {Provider} from "react-redux";
-import 'react-toastify/dist/Reacttoastify.css';
+// import 'react-toastify/dist/Reacttoastify.css';
 
 
 function App({ Component, pageProps:{session, ...pageProps}, }) {
